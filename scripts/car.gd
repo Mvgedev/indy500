ad05b783b3dccb948a766f2ddd87cfb9ccb64106
Mvge <mvge.dev@gmail.com>
2026-01-07 16:45:38 +0100

    Added Track Physics Layer and Terrain modifiers
    - Added Track on Physics Layer 1 (To handle out of road detection)
    - Added modifiers to each terrain type (Sand, Ice for now)
    - Make use of new local_speed/grip/friction... to update between road and terrain

--- scripts/car.gd
+++ scripts/car.gd
@@ -2,18 +2,54 @@ extends CharacterBody2D
 
 @onready var car_sprite: AnimatedSprite2D = $"Car Sprite"
 @onready var shadow: Sprite2D = $Shadow
-@onready var ray_cast_2d: RayCast2D = $RayCast2D
+
+@onready var pathing_raycast: RayCast2D = $"Pathing Raycast"
+
+# Wheels RC
+@onready var up_left_wheel_rc: RayCast2D = $"Wheels RC/UpLeftWheelRC"
+@onready var down_left_wheel_rc: RayCast2D = $"Wheels RC/DownLeftWheelRC"
+@onready var up_right_wheel_rc: RayCast2D = $"Wheels RC/UpRightWheelRC"
+@onready var down_right_wheel_rc: RayCast2D = $"Wheels RC/DownRightWheelRC"
+
+
 
 var h_brake = false
+# TRACK VALUE
 const TOP_SPEED = 200.0
-const ACCEL = 100.0
-const BRAKE_SPD = 150.0
-const HBRAKE_STEER = 1.2
-const HBRAKE_DRIFT_MOD = 0.8
 const REV_SPEED = -80.0
-const STEERING_SPD = 5
 const FRICTION_SPD = 150.0
 const GRIP = 550.0
+
+# SAND VALUE
+const SAND_SPEED = 20.0
+const SAND_REV = -15.0
+const SAND_FRICTION = 400.0
+const SAND_GRIP = 400.0
+
+# ICE VALUE
+const ICE_SPEED = 200.0
+const ICE_REV = -80.0
+const ICE_FRICTION = 10.0
+const ICE_GRIP = 30.0
+
+# Used Value
+var local_top_speed = TOP_SPEED
+var local_rev_speed = REV_SPEED
+var local_friction = FRICTION_SPD
+var local_grip = GRIP
+
+enum TERRAIN {SAND, ICE, DIRT, GRASS}
+var ground_terrain = TERRAIN.SAND
+
+const ACCEL = 100.0
+const BRAKE_SPD = 150.0
+
+const HBRAKE_STEER = 1.2
+const HBRAKE_DRIFT_MOD = 0.8
+
+const STEERING_SPD = 5
+
+# COLLISIONS
 const MAX_IMPACT = 200.0
 const IMP_BOUNCE = 0.25
 const MIN_SPEED_CRASH = 0.35
@@ -23,6 +59,9 @@ var lat_speed = 0.0
 var pace := CAR_PACE.STILL
 
 enum CAR_PACE {STILL, SLOW, MID, FAST, REVERSE}
+
+var on_road = true
+
 
 func _ready() -> void:
 	pass
@@ -37,10 +76,13 @@ func _input(event: InputEvent) -> void:
 func _physics_process(delta: float) -> void:
 	var throttle = Input.get_axis("brake","throttle")
 	var steering = Input.get_axis("steer_left","steer_right")
+	
+	var road_ratio = get_road_contact_ratio()
+	terrain_modifier(road_ratio)
 	# Handle Throttle speed
 	throttle_management(throttle, delta)
 	# Handle Steering
-	var speed_ratio = abs(cur_speed) / TOP_SPEED
+	var speed_ratio = abs(cur_speed) / local_top_speed
 	var steering_strength = steering_management(steering, speed_ratio)
 	if h_brake:
 		steering_strength *= HBRAKE_STEER
@@ -50,7 +92,7 @@ func _physics_process(delta: float) -> void:
 	var right = forward.orthogonal() # Slide motion
 	
 	lat_speed = velocity.dot(right)
-	var cur_grip = GRIP
+	var cur_grip = local_grip
 	if h_brake:
 		cur_speed = move_toward(cur_speed, 0.0, BRAKE_SPD * delta)
 		cur_grip *= HBRAKE_DRIFT_MOD
@@ -84,6 +126,25 @@ func _physics_process(delta: float) -> void:
 	# Animation
 	handle_pace(cur_speed)
 
+func terrain_modifier(road_ratio):
+	match ground_terrain:
+		TERRAIN.SAND:
+			local_friction = lerp(SAND_FRICTION, FRICTION_SPD, road_ratio)
+			local_grip = lerp(SAND_GRIP, GRIP, road_ratio)
+			local_top_speed = lerp(SAND_SPEED, TOP_SPEED, road_ratio)
+			local_rev_speed = lerp(SAND_REV, REV_SPEED, road_ratio)
+		TERRAIN.ICE:
+			local_friction = lerp(ICE_FRICTION, FRICTION_SPD, road_ratio)
+			local_grip = lerp(ICE_GRIP, GRIP, road_ratio)
+			local_top_speed = lerp(ICE_SPEED, TOP_SPEED, road_ratio)
+			local_rev_speed = lerp(ICE_REV, REV_SPEED, road_ratio)
+		TERRAIN.DIRT:
+			pass
+		TERRAIN.GRASS:
+			pass
+	
+	
+
 func steering_management(steering, speed_ratio) -> float:
 	if steering == 0.0:
 		return 0.0
@@ -95,8 +156,8 @@ func throttle_management(throttle, delta):
 	if throttle != 0:
 		cur_speed += throttle * ACCEL * delta
 	else:
-		cur_speed = move_toward(cur_speed, 0, FRICTION_SPD * delta)
-	cur_speed = clamp(cur_speed, REV_SPEED, TOP_SPEED)
+		cur_speed = move_toward(cur_speed, 0, local_friction * delta)
+	cur_speed = clamp(cur_speed, local_rev_speed, local_top_speed)
 
 
 func handle_pace(speed):
@@ -136,3 +197,11 @@ func define_pace(speed) -> CAR_PACE:
 		return CAR_PACE.MID
 	else:
 		return CAR_PACE.FAST
+
+func get_road_contact_ratio() -> float:
+	var hits := 0
+	var rays := [up_left_wheel_rc, up_right_wheel_rc, down_left_wheel_rc, down_right_wheel_rc]
+	for ray in rays:
+		if ray.is_colliding():
+			hits += 1
+	return hits / float(rays.size()) # Return 1.0 if all car on track, 0.75 if one wheel out, 0.5 if half out and so on
