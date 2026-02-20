f3857a5160e457fd83e1c4d7412ab3427966c68e
Mvge <mvge.dev@gmail.com>
2026-02-20 18:34:35 +0100

    Adding second circuit
    - Desert Dust circuit added for sand themed circuit

---
+++ scripts/Entities/circuit.gd
@@ -0,0 +1,7 @@
+extends Node2D
+class_name Circuit
+
+@onready var ground: TileMapLayer = $Ground
+@onready var track: TileMapLayer = $Track
+@onready var objectives: TileMapLayer = $Objectives
+@onready var walls: TileMapLayer = $Walls
